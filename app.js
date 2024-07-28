27192239541e457f06ec98a9e92a8dae58fcefc3
Sonalazar5 <sonalazar5@gmail.com>
2024-07-28 21:06:28 +0530

    user post and view api created

--- app.js
+++ app.js
@@ -27,6 +27,97 @@ app.post("/signup",async(req,res)=>{
     console.log(register)
     res.json({"status":"success"})
 })
+app.post("/signin", (req, res) => {
+    let input = req.body
+     registermodel.find({"email":req.body.email}).then(
+        (response)=>{
+           if (response.length>0) {
+            let dbPassword=response[0].password
+            console.log(dbPassword)
+            bcrypt.compare(input.password,dbPassword,(error,isMath)=>{
+                if (isMath) {
+                   jwt.sign({email:input.email},"blogg-app",{expiresIn:"1d"},(error,token)=>{
+                    if(error){
+                        res.json({"status":"unable to create token"})
+                    }else{
+                        res.json({"status":"success","userid":response[0]._id,"token":token})
+                    }
+                   })
+                } else {
+                    
+                    res.json("incorrect password")
+                }
+            })
+           } else {
+
+            res.json({"status":"user not found"})
+
+           }
+        }
+    ).catch()
+
+})
+
+app.post("/add",async(req,res)=>{
+    let input=req.body
+    let token=req.headers.token
+    jwt.verify(token,"blogg-app",async(error,decoded)=>{
+        if(decoded && decoded.email){
+            let result=new postModel(input)
+            await result.save()
+            res.json({"status":"success"})
+        }
+        else
+        {
+            res.json({"status":"Invalid Authentication"})
+        }
+    })
+})
+app.post("/viewall",(req,res)=>{
+    let token=req.headers.token
+    jwt.verify(token,"blogg-app",(error,decoded)=>{
+        if(decoded && decoded.email){
+            postModel.find().then(
+                (items)=>{
+                    res.json(items)
+                }
+            ).catch(
+                (error)=>{
+                    res.json({"status":"error"})
+                }
+            )
+            
+        }
+        else
+        {
+            res.json({"status":"Invalid Authentication"})
+        }
+    })
+})
+
+app.post("/viewmypost",(req,res)=>{
+    let input=req.body
+    let token=req.headers.token
+    jwt.verify(token,"blogg-app",(error,decoded)=>{
+        if(decoded && decoded.email){
+            postModel.find(input).then(
+                (items)=>{
+                    res.json(items)
+                }
+            ).catch(
+                (error)=>{
+                    res.json({"status":"error"})
+                }
+            )
+            
+        }
+        else
+        {
+            res.json({"status":"Invalid Authentication"})
+        }
+    })
+})
+
 app.listen(8080,()=>{
     console.log("server started")
 })
